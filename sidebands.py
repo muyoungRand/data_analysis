e7125648b619fda04d1866cd8ed890b5b4684503
randkim <kimmuyoung.97@gmail.com>
2022-11-11 13:25:54 +0800

    Updated sidebands.py

--- sidebands.py
+++ sidebands.py
@@ -124,10 +124,35 @@ def freq_diff(mode, threshold = 0.010):
         for j in range(i+1, len(mode)):
             val = mode[i][1] - mode[j][1]
 
-            if np.absolute(val) <= threshold and len(mode[i][0]) > 5:
+            if np.absolute(val) <= threshold and mode[i][0] != mode[j][0]:
                 differences.append([val, mode[i][0], mode[j][0]])
 
     return differences
+
+def freq_diff_modes(mode1, mode2, xmin, xmax, threshold = 0.010, plot = True):
+    differences = []
+
+    mode1 = [item for item in mode1 if item[1] >= xmin and item[1] <= xmax]
+    mode2 = [item for item in mode2 if item[1] >= xmin and item[1] <= xmax]
+
+    i = 0
+
+    while i < len(mode1):
+        for j in range(len(mode2)):
+            val = mode1[i][1] - mode2[j][1]
+
+            if np.absolute(val) <= 0.010 and mode1[i][0] != mode2[j][0]:
+                differences.append([val, mode1[i][0], mode2[j][0]])
+        
+        i += 1
+
+    if plot == True:
+        print("Overlapping 1st-2nd Order Peaks:")
+        for i in differences:
+            print('\tMode 1:', i[1], ", Mode 2:", i[2], ", Separtion =", np.round(i[0]*10**(3),1), 'kHz\n')
+
+    return differences
+
 ''' Frequency Calculations - 3 Ion Mode Frequencies + Higher Order Sidebands'''
 def calc_mode_freq(fax, fr1, fr2):
     '''
@@ -203,7 +228,7 @@ def third_order(f0, trap_freq):
 
 ''' Data Analysis and Plotting Functions '''
 
-def first_order_modes(mode, height, xmin, xmax):
+def first_order_modes(mode, height, xmin, xmax, plot = False):
     '''
     Plot all first-order modes for 3-ion case.
     Print all first-order modes for 3 ions that may be overlapping
@@ -215,20 +240,21 @@ def first_order_modes(mode, height, xmin, xmax):
     freq = [modes[i][1] for i in range(len(modes))]
     labels = [modes[i][0] for i in range(len(modes))]
 
-    for i in range(len(freq)):
-        if labels[i] in first_order_labels:
-            plt.vlines(freq[i], 0, height, color = 'r')
-            plt.text(freq[i]-0.03, height+0.05, labels[i] + '(' + str(round(f0[1] - freq[i], 4)) + ')', rotation = 90, fontsize = 'x-small', color = 'r')
-
     problems = freq_diff(modes)
+
+    if plot == True:
+        for i in range(len(freq)):
+            if labels[i] in first_order_labels:
+                plt.vlines(freq[i], 0, height, color = 'r')
+                plt.text(freq[i]-0.03, height+0.05, labels[i] + '(' + str(round(f0[1] - freq[i], 4)) + ')', rotation = 90, fontsize = 'x-small', color = 'r')
     
-    print("Overlapping Peaks:")
-    for i in problems:
-        print('\tMode 1:', i[1], ", Mode 2:", i[2], ", Separtion =", np.round(i[0]*10**(3),1), 'kHz\n')
+        print("First Order Overlapping Peaks:")
+        for i in problems:
+            print('\tMode 1:', i[1], ", Mode 2:", i[2], ", Separtion =", np.round(i[0]*10**(3),1), 'kHz\n')
 
     return problems
 
-def second_order_modes_1ion(mode, height, xmin, xmax):
+def second_order_modes_1ion(mode, height, xmin, xmax, plot = False):
     '''
     Plots 1-ion second order modes -> THe 3-ion second order modes are a complete mess.
     Prints any 3-ion modes that may be overlapping
@@ -238,26 +264,27 @@ def second_order_modes_1ion(mode, height, xmin, xmax):
     freq = [modes[i][1] for i in range(len(modes))]
     labels = [modes[i][0] for i in range(len(modes))]
 
-    for i in range(len(freq)):
-        if 'OP1' not in labels[i] and 'OP2' not in labels[i]:
-            plt.vlines(freq[i], 0, height, color = 'b')
-            plt.text(freq[i]+0.01, height+0.02, labels[i] + '(' + str(round(f0[1] - freq[i], 4)) + ')', rotation = 90, fontsize = 'x-small', color = 'b')
-
     problems = freq_diff(modes)
-    
-    print("Overlapping Peaks:")
-    for i in problems:
-        print('\tMode 1:', i[1], ", Mode 2:", i[2], ", Separtion =", np.round(i[0]*10**(3),1), 'kHz\n')
+
+    if plot == True:
+        for i in range(len(freq)):
+            if 'OP1' not in labels[i] and 'OP2' not in labels[i]:
+                plt.vlines(freq[i], 0, height, color = 'b')
+                plt.text(freq[i]+0.01, height+0.02, labels[i] + '(' + str(round(f0[1] - freq[i], 4)) + ')', rotation = 90, fontsize = 'x-small', color = 'b')
+
+        print("Second Order Overlapping Peaks:")
+        for i in problems:
+            if i[1] != i[2]:
+                print('\tMode 1:', i[1], ", Mode 2:", i[2], ", Separtion =", np.round(i[0]*10**(3),1), 'kHz\n')
 
     return problems
-
 #%%
-#freq, chn5, chn7, chn9 = read_file('freqscan globalraman 200us RepLock_-10dBm NoLock_5Amp 123p5_126p5.txt')
-#peaks, _ = find_peaks(chn5, height = 0.2, distance = 20, prominence = 0.08)
+plot = True
+
 xmin = 111.2
 xmax = 113.3
 
-plt.figure(dpi = 200)
+
 #plt.plot(freq, chn5, 'black')
 #plt.plot(freq[peaks], chn7[peaks], 'rx')
 
@@ -265,14 +292,22 @@ plt.figure(dpi = 200)
 #    if xmin < freq[i] < xmax:
 #        plt.text(freq[i] + 0.01, chn5[i]-0.1, str(round(f0[1] - freq[i], 4)), rotation = 90, fontsize = 'x-small')
 
-plt.xlim(xmin, xmax)
-
 f0 = ['', 113.20]
-guess_mode_freq = calc_mode_freq(0.462, 1.11, 1.57)
+ax = 0.462
+r1 = 1.55
+r2 = 2.01
 
-plt.axvline(x = f0[1])
-plt.xlabel('Frequency (MHz)')
-plt.ylabel("P(excited)")
+if plot == True:
+    plt.figure(dpi = 200)
+    plt.xlim(xmin, xmax)
+    plt.axvline(x = f0[1])
+    plt.xlabel('Frequency (MHz)')
+    plt.ylabel("P(excited)")
+
+valid_modes = []
+
+
+guess_mode_freq = calc_mode_freq(0.462, r1, r2)
 
 uncleansb1 = first_order(f0, guess_mode_freq)
 sb1 = []
@@ -288,8 +323,10 @@ for i in uncleansb3:
     supp = cleanup_labels(i)
     sb3.append(supp) """
 
-first_order_overlaps = first_order_modes(sb1, 0.8, xmin, xmax)
-second_order_overlaps = second_order_modes_1ion(sb2, 0.4, xmin, xmax)
+first_order_overlaps = first_order_modes(sb1, 0.8, xmin, xmax, plot)
+second_order_overlaps = second_order_modes_1ion(sb2, 0.4, xmin, xmax, plot)
+first_second_order_overlaps = freq_diff_modes(sb1, sb2, xmin, xmax, plot)
+
 
 #plt.rcParams["figure.figsize"] = (5, 5)
 #plot_counter_3ion = 0
@@ -300,5 +337,64 @@ second_order_overlaps = second_order_modes_1ion(sb2, 0.4, xmin, xmax)
 #plot_counter_1ion = plot_1ion_modes(sb1, 1.0, xmin, xmax, plot_counter_1ion)
 #plot_counter_1ion = plot_1ion_modes(sb2, 0.6, xmin, xmax, plot_counter_1ion)
 
-plt.show() 
+#plt.show() 
+
+#%%
+#freq, chn5, chn7, chn9 = read_file('freqscan globalraman 200us RepLock_-10dBm NoLock_5Amp 123p5_126p5.txt')
+#peaks, _ = find_peaks(chn5, height = 0.2, distance = 20, prominence = 0.08)
+plot = False
+
+xmin = 111.2
+xmax = 113.3
+
+
+#plt.plot(freq, chn5, 'black')
+#plt.plot(freq[peaks], chn7[peaks], 'rx')
+
+#for i in peaks:
+#    if xmin < freq[i] < xmax:
+#        plt.text(freq[i] + 0.01, chn5[i]-0.1, str(round(f0[1] - freq[i], 4)), rotation = 90, fontsize = 'x-small')
+
+f0 = ['', 113.20]
+ax = 0.462
+r1 = [1.11 - 0.01 * i for i in range(-50, 50)]
+r2 = [1.57 - 0.01 * i for i in range(-50, 50)]
+
+
+if plot == True:
+    plt.figure(dpi = 200)
+    plt.xlim(xmin, xmax)
+    plt.axvline(x = f0[1])
+    plt.xlabel('Frequency (MHz)')
+    plt.ylabel("P(excited)")
+
+valid_modes = []
+
+for k in range(len(r1)):
+    guess_mode_freq = calc_mode_freq(0.462, r1[k], r2[k])
+
+    uncleansb1 = first_order(f0, guess_mode_freq)
+    sb1 = []
+    for i in uncleansb1:
+        sup = cleanup_labels(i)
+        sb1.append(sup)
+
+    sb2 = second_order(f0, guess_mode_freq)
+
+    """ uncleansb3 = third_order(f0, guess_mode_freq)
+    sb3 = []
+    for i in uncleansb3:
+        supp = cleanup_labels(i)
+        sb3.append(supp) """
+
+    first_order_overlaps = first_order_modes(sb1, 0.8, xmin, xmax, plot)
+    second_order_overlaps = second_order_modes_1ion(sb2, 0.4, xmin, xmax, plot)
+    first_second_order_overlaps = freq_diff_modes(sb1, sb2, xmin, xmax)
+
+    if len(first_order_overlaps) == 0 and len(first_second_order_overlaps) < 3 and len(second_order_overlaps) < 10:
+        valid_modes.append([r1[k], r2[k]])
+
+print("Possible Modes:", valid_modes)
+
+
 # %%
